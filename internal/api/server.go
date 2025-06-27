2beefc0aebeb7fc6b78ff17e518fa3c0ef7b4b5a
Kunal Singh <kunalsin9h@gmail.com>
2025-06-27 22:02:17 +0530

    refactor: http apis

---
+++ internal/api/server.go
@@ -0,0 +1,46 @@
+package api
+
+import (
+	"fmt"
+	"net/http"
+
+	"k8s.io/client-go/kubernetes"
+)
+
+type ServerConfig struct {
+	Host      string
+	Port      string
+	Env       string // DEV or PROD
+	ClientSet *kubernetes.Clientset
+}
+
+func NewServiceConfig(host, port, env string, clientSet *kubernetes.Clientset) *ServerConfig {
+	return &ServerConfig{
+		Host:      host,
+		Port:      port,
+		Env:       env,
+		ClientSet: clientSet,
+	}
+}
+
+func StartHttpServer(config *ServerConfig) error {
+	mux := http.NewServeMux()
+
+	// Heath check endpoint
+	mux.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
+	// Application endpoints
+	mux.HandleFunc("GET /", config.WebHome)
+	mux.HandleFunc("POST /restart", config.RestartDeployment)
+	mux.HandleFunc("POST /update-image", config.UpdateDeploymentImage)
+
+	err := http.ListenAndServe(config.Host+":"+config.Port, mux)
+	if err != nil {
+		return fmt.Errorf("failed to start server: %v", err)
+	}
+
+	return nil
+}
--- main.go
+++ main.go
@@ -3,11 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 
+	"github.com/kunalsin9h/upkube/internal/api"
 	"github.com/kunalsin9h/upkube/internal/kubeapi"
-	"github.com/kunalsin9h/upkube/views"
 )
 
 var (
@@ -34,78 +33,10 @@ func main() {
 		log.Fatal(fmt.Errorf("failed to create Kubernetes clientset: %v", err))
 	}
 
-	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		// Pass the request or extracted info to your component
-		userEmail := r.Header.Get("Cf-Access-Authenticated-User-Email")
-		// If your Root templ expects userEmail:
+	config := api.NewServiceConfig(UPKUBE_HOST, UPKUBE_PORT, UPKUBE_ENV, clientSet)
 
-		// TODO: unauthorized page.
-		// if userEmail == "" || true {
-		// 	// Not authenticated or header missing
-		// 	http.Error(w, "Unauthorized", http.StatusUnauthorized)
-		// 	return
-		// }
-
-		userEmail = "kunal.singh@safedep.io"
-
-		namespace := r.URL.Query().Get("namespace")
-		if namespace == "" {
-			namespace = "default"
-		}
-
-		root := views.Root(userEmail, clientSet, namespace)
-		root.Render(r.Context(), w)
-	})
-
-	http.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("OK"))
-	})
-
-	http.HandleFunc("POST /restart", func(w http.ResponseWriter, r *http.Request) {
-		namespace := r.FormValue("namespace")
-		deployment := r.FormValue("deployment")
-		if namespace == "" || deployment == "" {
-			http.Error(w, "Missing parameters", http.StatusBadRequest)
-			return
-		}
-		fmt.Println(namespace, deployment)
-		// TODO: Send some notification to the user.
-		err := kubeapi.RestartDeployment(clientSet, namespace, deployment)
-		if err != nil {
-			http.Error(w, "Failed to restart deployment: "+err.Error(), http.StatusInternalServerError)
-			return
-		}
-		http.Redirect(w, r, "/", http.StatusSeeOther)
-	})
-
-	http.HandleFunc("POST /update-image", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodPost {
-			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-			return
-		}
-
-		namespace := r.FormValue("namespace")
-		deployment := r.FormValue("deployment")
-		imagePrefix := r.FormValue("imagePrefix")
-		oldTag := r.FormValue("oldTag")
-		tag := r.FormValue("tag")
-
-		if namespace == "" || deployment == "" || oldTag == "" || imagePrefix == "" || tag == "" {
-			http.Error(w, "Missing parameters", http.StatusBadRequest)
-			return
-		}
-
-		newImage := imagePrefix + ":" + tag
-
-		err := kubeapi.UpdateDeploymentImage(clientSet, namespace, deployment, newImage)
-		if err != nil {
-			http.Error(w, "Failed to update image: "+err.Error(), http.StatusInternalServerError)
-			return
-		}
-		http.Redirect(w, r, r.Header.Get("Referer"), http.StatusSeeOther)
-	})
-
-	fmt.Println("Listening on http://" + UPKUBE_HOST + ":" + UPKUBE_PORT)
-	http.ListenAndServe(UPKUBE_HOST+":"+UPKUBE_PORT, nil)
+	log.Printf("Starting Upkube server on %s:%s in %s environment", config.Host, config.Port, config.Env)
+	if err := api.StartHttpServer(config); err != nil {
+		log.Fatal(fmt.Errorf("failed to start HTTP server: %v", err))
+	}
 }
